e0149e66a107dc367b4b762b1c8178a09f78e81c
Anton Timaschew <timaschew@gmail.com>
2009-08-04 21:47:04 +0200

    zwischenabfrage mit neuer view: Zeitlimit für Kurse einstellen, die zeitlich verschoben werden müssen, code aufräumen

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,69 +1,89 @@
 class SchedulesController < ApplicationController
-  include SchedulesHelper
-  @smallFreeRooms = nil         # freie Räume, die aber zu klein sind in Schritt (A)
-  @timeTableList = nil          # alle Kurse mit Informationen als Hash
-  @messages = nil               # zum testen
+
+  $smallFreeRooms = nil         # freie Räume, die aber zu klein sind in Schritt (A)
+  $timeTableList = nil          # alle Kurse mit Informationen als Hash
   
 	def index
 		@rooms = Room.find(:all)
 	end
+	
+	def continueChecking
+	  
+	  @parameter = params
+	  
+	  
+	  $scheduleBCourses.each_with_index do |cID, i| 
+	    tmpHash = {}
+	    tmpHash[:course] = cID
+	    tmpHash[:limitStartHour] = params[:time][""+cID.to_s+"_start(4i)"] # Anfangszeitpunkt der Belegung (Stunde)
+  		tmpHash[:limitStartMin] = params[:time][""+cID.to_s+"_start(5i)"]  # Anfangszeitpunkt der Belegung (Minute)
+  		tmpHash[:limitEndHour] = params[:time][""+cID.to_s+"_end(4i)"]     # Endzeitpunkt der Belegung (Stunde)
+  		tmpHash[:limitEndMin] = params[:time][""+cID.to_s+"_end(5i)"]      # Endzeitpunkt der Belegung (Minute)
+	    $scheduleBCourses[i] = tmpHash
+	  end
+	  
+  end
+  
 
 	def check
-	  #ActiveRecord::Base.logger = Log4r::Logger.new("Application Log") 
-	  #ActiveRecord::Base.logger.level = 0 # at any time 
 	  
-	  ActiveRecord::Base.logger = Logger.new("Application Log") 
+	  ActiveRecord::Base.logger = Logger.new("log/schedules_controller.log") 
     ActiveRecord::Base.logger.level = 0 # warn 
-    
 	  
-		@submit = params[:commit]
+	  submitForm = params[:commit]
 		
 		# Prüfe ob Formular benutzt wurde
-		if @submit == "choose"
+		if submitForm == "choose"
 			
 			# Parameter aus dem Forumlar auslesen
-			@roomID = params[:room_id].to_i   # Raum der nun belegt ist
-			@date = params[:date]             # Datum
-			@day = @date[0,2]                 # Tag
-			@month = @date[3,2]               # Monat
-			@year = @date[6,4]                # Jahr
-			@date = Date.new(@year.to_i, @month.to_i, @day.to_i) # echtes Datum
-			@weekdayNo = @date.wday.to_i      # Wochentag als Zahl
-			@weekday = @date.strftime("%A")   # Wochentag als Name
+			$busyRoomID = params[:room_id].to_i   # Raum der nun belegt ist
+			date = params[:date]             # Datum
+			day = date[0,2]                 # Tag
+			month = date[3,2]               # Monat
+			year = date[6,4]                # Jahr
+			date = Date.new(year.to_i, month.to_i, day.to_i) # echtes Datum
+			$weekday = date.wday.to_i      # Wochentag als Zahl
+			$weekdayString = date.strftime("%A")   # Wochentag als Name
 			
-			@startHour = params[:time]["start(4i)"] # Anfangszeitpunkt der Belegung (Stunde)
-			@startMin = params[:time]["start(5i)"]  # Anfangszeitpunkt der Belegung (Minute)
-			@endHour = params[:time]["end(4i)"]     # Endzeitpunkt der Belegung (Stunde)
-			@endMin = params[:time]["end(5i)"]      # Endzeitpunkt der Belegung (Minute)
+			$busyStartH = params[:time]["start(4i)"] # Anfangszeitpunkt der Belegung (Stunde)
+			$busyStartM = params[:time]["start(5i)"]  # Anfangszeitpunkt der Belegung (Minute)
+			$busyEndH = params[:time]["end(4i)"]     # Endzeitpunkt der Belegung (Stunde)
+			$busyEndM = params[:time]["end(5i)"]      # Endzeitpunkt der Belegung (Minute)
 			
 			# Alle Kurse an dem Tag in das Objekt @container laden
-			init(@weekdayNo)
-			@container = @timeTableList # getTimeTable
-			@originalTimeTable = @container.dup
+			getAllCoursesAtWeekday($weekday)
+			$timeTableList # getTimeTable
+			originalTimeTable = $timeTableList.dup
 			
-			@smallFreeRooms = []    # Kurse die im Schritt (A) verschoben werden (gleiche Zeit, anderer Raum)
-			@scheduleBCourses = []  # Kurse die im Schritt (B) verschoben werden (gleicher Raum, andere Zeit)
-			@scheduleCCourses = []  # Kurse die im Schritt (C) verschoben werden (anderer Raum, andere Zeit)
+			$smallFreeRooms = []
 			
-			# Pruefe welche Kurse fuer in dem Zeitraum und Raum liegen und gebe Kurs-IDs als Array zurück
-			@scheduleCourses = getCoursesToSchedule(@roomID, @weekdayNo, @startHour, @startMin, @endHour, @endMin)
+			$scheduleCourses # Kurse die im Schritt (A) verschoben werden (gleiche Zeit, anderer Raum)
+			$scheduleBCourses = []  # Kurse die im Schritt (B) verschoben werden (gleicher Raum, andere Zeit)
+			$scheduleCCourses = []  # Kurse die im Schritt (C) verschoben werden (anderer Raum, andere Zeit)
+			
+			# Pruefe welche Kurse in dem busy-Zeitraum und busy-Raum liegen und gebe Kurs-IDs als Array zurück
+			$scheduleCourses = getCoursesToSchedule()
 			
 			# SCHRITT (A)
 			# Fuer jeden Kurs der verschoben werden muss, suche einen freien Raum, zur gleichen Zeit
-			@scheduleCourses.each_with_index do |cID, i|
+			$scheduleCourses.each_with_index do |cID, i|
 			  course = Course.find(cID) # komplettes Objekt mit allen Feldern
-				size = Courselist.find_all_by_course_id(cID).count   # Größe des Kurses bestimmen
-				newRoom = checkFreeRoomAtSameTime(@weekdayNo, size, course.id, course.start, course.duration, @roomID)
+				#size = Courselist.find_all_by_course_id(cID).count   # Größe des Kurses bestimmen
+				size = 10
+				newRoom = checkFreeRoomAtSameTime(size, course.id, course.start, course.duration)
 				if !newRoom.nil? && newRoom != false
 					changeRoom(course.id, newRoom)
-					#@scheduleCourses.delete_at(i)
 				else
-				  @scheduleBCourses.push(cID)
-					# Raum bleibt in @scheduleCourses und es wirt Schritt 3b angewandt
+				  $scheduleBCourses.push(cID)
+					# Raum bleibt in $scheduleCourses und es wirt Schritt 3b angewandt
 				end
 			end # Schritt (A)
 			
-			@scheduleBCourses.each_with_index do |cID, i|
+			if !$scheduleBCourses.empty?
+			  render :template => "schedules/setTimeDistance"
+		  end
+			
+			$scheduleBCourses.each_with_index do |cID, i|
 			  offset = 3600 # init (Stundenweise), alternativ 1800 oder 900 (halbe Stunde oder viertel Stunde)
 			  limit = 3 # Limit (immer in Stunden, 0,5 = halbe Stunde, 0.25 = viertel Stunde)
 			  range = limit * (3600/offset)
@@ -76,15 +96,15 @@ class SchedulesController < ApplicationController
 			  time = getForCourse(course.id)[:startTime].utc
 			  
 			  logger.debug ""
-			  logger.debug "#{@timeTableList.inspect}"
+			  logger.debug "#{$timeTableList.inspect}"
 			  logger.debug ""
 			  
 			  range.times do |i|
 			    newStartTime = time + offset
-			    blockingTimeStart = Time.mktime(0, 1, 1, @startHour.to_i + 1, @startMin.to_i).utc
-			    blockingTimeEnd = Time.mktime(0, 1 , 1, @endHour.to_i + 1, @endMin.to_i).utc
+			    busyTimeStart = Time.mktime(0, 1, 1, $busyStartH.to_i + 1, $busyStartM.to_i).utc
+			    busyTimeEnd = Time.mktime(0, 1 , 1, $busyEndH.to_i + 1, $busyEndM.to_i).utc
 			    # den selben Raum nur prüfen, wenn es außerhalb des besetzten Zeitraums liegt
-			    if !newStartTime.between?(blockingTimeStart, blockingTimeEnd)
+			    if !newStartTime.between?(busyTimeStart, busyTimeEnd)
   			    logger.debug "test #{newStartTime}"
   			    result = checkRoomAtOtherTime(course.id, newStartTime)
   			    if !result.nil? && result == 1
@@ -103,9 +123,8 @@ class SchedulesController < ApplicationController
 		      end
 		    end
 			  
-			  
 			  if changed == false
-			    @scheduleCCourses.push(cID)
+			    $scheduleCCourses.push(cID)
 		    end
 			  
 			end
@@ -133,10 +152,11 @@ class SchedulesController < ApplicationController
   	stattfinden.
   	Diese werden in einem Array gespeichert und als Rückgabewert übergeben
 =end
-    def getCoursesToSchedule(roomID, weekday, startHour, startMin, endHour, endMin)
+
+    def getCoursesToSchedule()
   		#setze Zeit zusammen
-  		startTime = startHour + ":" + startMin
-  		endTime = endHour + ":" + endMin
+  		busyStartTime = $busyStartH + ":" + $busyStartM
+  		busyEndTime = $busyEndH + ":" + $busyEndM
   		courseList = Course.find(
   			:all,
   			:conditions => [
@@ -149,10 +169,10 @@ class SchedulesController < ApplicationController
   					OR
   					(start < ? AND duration > ?)
   				)",
-  				roomID, weekday, 
-  				startTime, endTime, startTime, endTime,
-  				startTime, startTime,
-  				endTime, endTime
+  				$busyRoomID, $weekday, 
+  				busyStartTime, busyEndTime, busyStartTime, busyEndTime,
+  				busyStartTime, busyStartTime,
+  				busyEndTime, busyEndTime
   			],
   			:order => "start"
   		).map(&:id)
@@ -164,14 +184,14 @@ class SchedulesController < ApplicationController
   einen Ersatzraum, der frei ist, außer notRoom (dieser ist nämlich gesperrt)
   Wenn ein Raum gefunden gib Raumnumme zurück, sonst -1
 =end
-  	def checkFreeRoomAtSameTime(weekday, size, courseID, startDate, endDate, notRoom) 
+  	def checkFreeRoomAtSameTime(size, courseID, startDate, endDate) 
   		#startTime = startDate.strftime("%H") + ":" + startDate.strftime("%M")
   	  #endTime = endDate.strftime("%H")  + ":" + endDate.strftime("%M") 
   		freeRoom = nil
   		
   		allRooms = Room.find(:all, :order => "size").map(&:id)
   		allRooms.each do |r|
-  		  if r == notRoom
+  		  if r == $busyRoomID
   		    next 
 		    end
   		  
@@ -187,7 +207,7 @@ class SchedulesController < ApplicationController
   				  freeRoom = r
   			  else
   			    # Raum ist zu klein, aber merke es evtl für später
-  			    @smallFreeRooms.push(Hash.[](
+  			    $smallFreeRooms.push(Hash.[](
   			    :start, Time.mktime(0, 1, 1, startDate.strftime("%H").to_i + 1, startDate.strftime("%M").to_i).utc, 
   			    :end, Time.mktime(0, 1, 1, endDate.strftime("%H").to_i + 1, endDate.strftime("%M").to_i).utc, 
   			    :room, r))
@@ -230,11 +250,11 @@ class SchedulesController < ApplicationController
 
 =begin
   Prüfe ob zu dem jeweiligen Zeitraum im jeweiligen Raum schon ein Kurs stattfindet
-  auf Basis der Daten in @timeTableList
+  auf Basis der Daten in $timeTableList
 =end
   def checkNewTimeAndRoom(r, startDate, endDate)
     alreadyCourses = []
-    @timeTableList.each do |l|
+    $timeTableList.each do |l|
       #logger.debug "#{r.to_s} ==  #{l[:room].to_s}"
       if r == l[:room]  # Prüfe auf gleichen Raum
         #logger.debug "\tTRUE: #{r.to_s} ==  #{l[:room].to_s}"
@@ -250,6 +270,83 @@ class SchedulesController < ApplicationController
     end # Schleife
     #logger.debug "1. Laenge: #{alreadyCourses.count}"
     alreadyCourses
+  end
+
+=begin
+  Instanzvariablen werden initialisiert
+  und es werden alle Kurse für den Tag weekday in die $timeTableList geladen
+=end
+  def getAllCoursesAtWeekday(weekday)
+    $timeTableList = []
+    courses = Course.find(:all, :conditions => {:weekday => weekday}, :order => :start)
+    courses.each do |c|
+      insertCourse(
+        c.id, c.room_id, 
+        c.start.strftime("%H"), 
+        c.start.strftime("%M"), 
+        c.duration.strftime("%H"), # end
+        c.duration.strftime("%M")) # end
+    end
+	  #logger.debug "#{$timeTableList.inspect}"
+  end
+
+=begin
+  Jedes Element in $timeTableList hat einen Hash mit Informationen zu Kurs-ID, Raum-ID, Zeitraum (Anfang und Ende)
+=end
+  def insertCourse(courseID, roomID, startHour, startMin, endHour, endMin)
+    entry = {}
+    entry[:course] = courseID
+    entry[:room] = roomID
+    entry[:startTime] = Time.mktime(0, 1, 1, (startHour.to_i + 1), startMin).utc
+    entry[:endTime] = Time.mktime(0, 1, 1, (endHour.to_i + 1), endMin).utc
+    #entry[:startHour] = startHour
+    #entry[:startMin] = startMin
+    #entry[:endHour] = endHour
+    #entry[:endMin] = endMin
+    $timeTableList.push(entry)
+  end
+
+=begin
+  Es wird das Element (Hash) mit dem passenden Kurs zurückgegeben, 
+  wird per Kurs-ID bestimmt
+=end
+  def getForCourse(courseID)
+    val = -1
+    $timeTableList.each do |l|
+      if l[:course] == courseID
+        val = l
+        break
+      end
+    end
+    val # return value
+  end
+
+=begin
+  die $timeTableList wird verändert, der entsprechende Kurs bekommt einen neuen Raum zugewiesen
+=end
+  def changeRoom(cID, rID)
+    $timeTableList.each_with_index do |t, i|
+      if cID.to_i == t[:course].to_i
+        #@messages.push("Changing Room: Kurs " + cID.to_s + " von " + t[:room].to_s + " nach " + rID.to_s)
+        logger.debug "Changing Room: Kurs #{cID.to_s} von #{t[:room].to_s}  nach #{rID.to_s}"
+        $timeTableList[i][:room] = rID
+        $timeTableList[i][:changed] = 1
+      end
+    end
+  end
+
+=begin
+  die $timeTableList wird verändert, der entsprechende Kurs bekommt eine neue Zeit zugewiesen
+=end
+  def changeTime(cID, startTime)
+    $timeTableList.each_with_index do |t, i|
+      if cID.to_i == t[:course].to_i
+        #@messages.push("Changing Room: Kurs " + cID.to_s + " von " + t[:room].to_s + " nach " + rID.to_s)
+        logger.debug "Changing Time: Kurs #{cID.to_s} von #{t[:startTime].to_s} nach #{startTime})"
+        $timeTableList[i][:startTime] = startTime
+        $timeTableList[i][:changed] = 1
+      end
+    end
   end
 
 end
--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,66 +1,13 @@
 module SchedulesHelper
   
 =begin
-  Instanzvariablen werden initialisiert
-  und es werden alle Kurse für den Tag weekday in die @timeTableList geladen
-=end
-  def init(weekday)
-    @timeTableList = []
-    @timeTableCount = []
-    @messages = []
-    courses = Course.find(:all, :conditions => {:weekday => weekday}, :order => :start)
-    courses.each do |c|
-      insertCourse(
-        c.id, c.room_id, 
-        c.start.strftime("%H"), 
-        c.start.strftime("%M"), 
-        c.duration.strftime("%H"), # end
-        c.duration.strftime("%M")) # end
-    end
-	  #logger.debug "#{@timeTableList.inspect}"
-  end
-  
-=begin
-  Jedes Element in @timeTableList hat einen Hash mit Informationen zu Kurs-ID, Raum-ID, Zeitraum (Anfang und Ende)
-=end
-  def insertCourse(courseID, roomID, startHour, startMin, endHour, endMin)
-    entry = {}
-    entry[:course] = courseID
-    entry[:room] = roomID
-    entry[:startTime] = Time.mktime(0, 1, 1, (startHour.to_i + 1), startMin).utc
-    entry[:endTime] = Time.mktime(0, 1, 1, (endHour.to_i + 1), endMin).utc
-    #entry[:startHour] = startHour
-    #entry[:startMin] = startMin
-    #entry[:endHour] = endHour
-    #entry[:endMin] = endMin
-    @timeTableList.push(entry)
-  end
-  
-=begin
-  Es wird das Element (Hash) mit dem passenden Kurs zurückgegeben, 
-  wird per Kurs-ID bestimmt
-=end
-  def getForCourse(courseID)
-    val = -1
-    @timeTableList.each do |l|
-      if l[:course] == courseID
-        val = l
-        break
-      end
-    end
-    val # return value
-  end
-  
-  
-  
-=begin
     Es wird das Element (Hash) mit dem passenden Kurs zurückgegeben,
     wird per Zeit und Raum bestimmt (nur mit dieser Kombination ist der Kurs eindeutig)
 =end
   def getForStartTimeAndRoom(startHour, startMin, roomID)
     startTime = Time.mktime(0, 1, 1, (startHour + 1), startMin).utc
     val = nil
-    @timeTableList.each do |l|
+    $timeTableList.each do |l|
       if l[:room].to_i == roomID.to_i && l[:startTime] == startTime
         #logger.debug "Zeitvergleich: true; Raum #{roomID}; Kurs #{l[:course]}" 
         val = l
@@ -68,35 +15,6 @@ module SchedulesHelper
       end
     end
     val # return
-  end
-  
-  
-=begin
-  die @timeTableList wird verändert, der entsprechende Kurs bekommt einen neuen Raum zugewiesen
-=end
-  def changeRoom(cID, rID)
-    @timeTableList.each_with_index do |t, i|
-      if cID.to_i == t[:course].to_i
-        #@messages.push("Changing Room: Kurs " + cID.to_s + " von " + t[:room].to_s + " nach " + rID.to_s)
-        logger.debug "Changing Room: Kurs #{cID.to_s} von #{t[:room].to_s}  nach #{rID.to_s}"
-        @timeTableList[i][:room] = rID
-        @timeTableList[i][:changed] = 1
-      end
-    end
-  end
-
-=begin
-  die @timeTableList wird verändert, der entsprechende Kurs bekommt eine neue Zeit zugewiesen
-=end
-  def changeTime(cID, startTime)
-    @timeTableList.each_with_index do |t, i|
-      if cID.to_i == t[:course].to_i
-        #@messages.push("Changing Room: Kurs " + cID.to_s + " von " + t[:room].to_s + " nach " + rID.to_s)
-        logger.debug "Changing Time: Kurs #{cID.to_s} von #{t[:startTime].to_s} nach #{startTime})"
-        @timeTableList[i][:startTime] = startTime
-        @timeTableList[i][:changed] = 1
-      end
-    end
   end
   
 end
